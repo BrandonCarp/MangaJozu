3bad12b95f0e6f95a820cd039765bc5d3da8a07b
Brandon <brancarp93@gmail.com>
2023-10-14 22:00:39 -0400

    update .env

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,23 @@
-const express = require("express");
+import express, {NextFunction, Request, Response} from "express";
+require("dotenv").config();
+
+const DEV_PORT = process.env.DEV_PORT || 7000;
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+}));
+
+
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
+  next()
+})
+
+app.listen(DEV_PORT, () => {
+  console.log(`Application is listening on http://localhost:${DEV_PORT}`)
+})
