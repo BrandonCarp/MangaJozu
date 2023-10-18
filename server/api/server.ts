3e058142ce7c4edab978e770438baca507e65e53
Brandon <brancarp93@gmail.com>
2023-10-17 23:22:48 -0400

    backend api created

--- server/api/server.ts
+++ server/api/server.ts
@@ -1,23 +1,28 @@
-import express, {NextFunction, Request, Response} from "express";
+import express, { NextFunction, Request, Response } from "express";
 require("dotenv").config();
 
 const DEV_PORT = process.env.DEV_PORT || 7000;
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-  origin: ['http://localhost:3000'],
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-}));
-
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use(function (req: Request, res: Response, next: NextFunction) {
-  res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
-  next()
-})
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
+  next();
+});
 
 app.listen(DEV_PORT, () => {
-  console.log(`Application is listening on http://localhost:${DEV_PORT}`)
-})
+  console.log(`Server working on on http://localhost:${DEV_PORT}`);
+});
+
+app.get("/api/home", (req: Request, res: Response) => {
+  res.json({ Brandon: "Yo Panini you a whole PUSSY" });
+});
