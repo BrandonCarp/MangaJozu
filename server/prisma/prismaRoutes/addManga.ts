70bf900ab57a8f43ae5a66a9fc0b8619d4f72cc1
Brandon <brancarp93@gmail.com>
2023-11-05 21:07:36 -0500

    added comment in addManga.ts

--- server/prisma/prismaRoutes/addManga.ts
+++ server/prisma/prismaRoutes/addManga.ts
@@ -40,6 +40,7 @@ const formatManga = (data: MangaListItem) => {
   };
 };
 
+// Switch to anime site or stay manga ?
 async function addAnime() {
 
     const fetchManga = process.env.FETCH_MANGA;
@@ -59,26 +60,26 @@ async function addAnime() {
 addAnime();
 
 
-async function addManga() {
-  try {
-    const fetchManga = process.env.FETCH_MANGA;
-    const response = await axios.get(fetchManga);
+// async function addManga() {
+//   try {
+//     const fetchManga = process.env.FETCH_MANGA;
+//     const response = await axios.get(fetchManga);
 
-    const apiData = response.data.data; // Access the 'data' field from the response
+//     const apiData = response.data.data; // Access the 'data' field from the response
 
-    if (Array.isArray(apiData)) {
-      apiData.forEach((object) => {
-        console.log(object); // Logging each manga object retrieved from the API
-        // Perform operations with each manga object as needed
-      });
-    } else {
-      console.error('Data is not in the expected array format');
-      console.log(apiData); // Check the structure of the received 'data' field
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     if (Array.isArray(apiData)) {
+//       apiData.forEach((object) => {
+//         console.log(object); // Logging each manga object retrieved from the API
+//         // Perform operations with each manga object as needed
+//       });
+//     } else {
+//       console.error('Data is not in the expected array format');
+//       console.log(apiData); // Check the structure of the received 'data' field
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
 // // Make sure to call the addManga function to execute the API call and processing
 // addManga();
