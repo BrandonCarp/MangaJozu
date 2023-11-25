279824ae005f6c117913cdea95bf7c539245f7dd
Brandon <brancarp93@gmail.com>
2023-11-25 18:06:08 -0500

    createCustomer file error handling, neeed Auth0

--- server/prisma/controllers/customerFunctions/createCustomer.ts
+++ server/prisma/controllers/customerFunctions/createCustomer.ts
@@ -7,6 +7,7 @@ import { CustomerDetails } from "../../../api/interface";
 // create function that creates new customer
 // Must check that user is not duplicate
 // utilize JWT 
+// Auth0 
 
 
 // Customer example
@@ -20,18 +21,34 @@ lastName: "Carpenter"
 
 
 
-async function createCustomer ({userName, email, password, firstName, lastName }: CustomerDetails) {
-  await prisma.customer.create({
- data: {
-userName: userName,
-email: email,
-password: password,
-firstName: firstName,
-lastName: lastName
- } 
-  })
+async function createCustomer ({userName, email, password, firstName, lastName}: CustomerDetails) {
+
+  try {
+
+    if (!userName || !email || !password || !firstName || !lastName) {
+      throw new Error('Incomplete customer details');  
+    }
+
+
+    await prisma.customer.create({
+      data: {
+     userName: userName,
+     email: email,
+     password: password,
+     firstName: firstName,
+     lastName: lastName
+      } 
+
+
+       })
+
+  } catch (error) {
+    console.log(`Error creating customer:`, error)
+  }
+  
 }
 
 
 createCustomer(customer);
 
+ 
