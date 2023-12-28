9fb4a43278c71857cfd602f54a01adf26d43e218
Brandon <brancarp93@gmail.com>
2023-12-27 21:03:08 -0500

    updatedUser consoller function

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -18,8 +18,6 @@ async function createCustomer ({userId, userName, email, password, firstName, la
      firstName: firstName,
      lastName: lastName
       } 
-
-
        })
 console.log(createdUser)
   } catch (error) {
@@ -42,7 +40,31 @@ async function deleteUser({userId}: CustomerDetails) {
   }
 }
 
+async function updateUser({userId, userName, email, password, firstName, lastName}: CustomerDetails) {
+ try {
+   const updatedUser = await prisma.customer.update({
+    where: {
+      id: userId
+    },
+    data: {
+      userName: userName,
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName
+    }
+   })
+   console.log(`Updated User`, updatedUser)
+} catch (error){
+  console.log(`Error updating customer:`, error)
+}
+}
 
+module.exports = {
+createCustomer,
+deleteUser,
+updateUser
+}
 // create multiple functions for adding/delete/updating users
 // including update address/information - potentially different file
 // 12/18/23   - Create a delete function & a edit function
